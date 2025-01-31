06edf12b3c2222c3961061f5e64c9e9fa950d0b4
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2025-01-31 22:02:33 +0600

    added module css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import ReachOutPortion from './components/reachOutPortion/reachOutPortion'
+import ReachOutPortion from './components/reachOutPortion/ReachOutPortion'
 
 function App() {
   const [count, setCount] = useState(0)
--- src/components/reachOutPortion/ReachOutPortion.jsx
+++ src/components/reachOutPortion/ReachOutPortion.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./reachOutPortion.module.css"
 
 const ReachOutPortion = () => {
     return (
-        <div className=''>
-            <div className='w-96 bg-[url(./assets/backgrounds/backgrounds/WaveLinesDesktop1.svg)]'>
+        <div className={`${styles.headerPart} w-screen `}>
+            <div>
                 <h1 className='underline'>Embrace the future of finance</h1>
                 <p>Reimagine financial services with AnyTech’s open platform, distributed banking solution that powers transformation</p>
                 <button type="button">Reach out to us</button>
